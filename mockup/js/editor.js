7453756fa2394535fae1116a7564717693e37af8
liaoyiming <me@liaoyiming.com>
2015-03-12 01:00:24 +0800

    Fix

--- mockup/js/editor.js
+++ mockup/js/editor.js
@@ -22,22 +22,4 @@ $(document).ready(function(e) {
 		$(this).toggleClass("icon-screen-split");
 		$(this).toggleClass("btn-screen-split");
 	});
-	$("#nav-menu").mouseover(function() {
-		$("#nav-newfile").show();
-		$("#nav-newfile").animate({top: "65px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-newfolder").show();
-		$("#nav-newfolder").animate({top: "120px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-setting").show();
-		$("#nav-setting").animate({top: "175px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-exit").show();
-		$("#nav-exit").animate({top: "230px", opacity: 1}, 200, "easeInCubic");
-		return(false);
-	});
-	$("#nav-wrapper").mouseleave(function() {
-		$("#nav-newfile").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-newfile").hide()});
-		$("#nav-newfolder").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-newfolder").hide()});
-		$("#nav-setting").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-setting").hide()});
-		$("#nav-exit").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-exit").hide()});
-		return(false);
-	});
 });
--- mockup/js/list.js
+++ mockup/js/list.js
@@ -33,28 +33,4 @@ $(document).ready(function(e) {
 	$(".file").mouseleave(function() {
 		$(this).children(".file-menu").hide();
 	});
-	$("#nav-menu").mouseover(function() {
-		$("#nav-newfile").show();
-		$("#nav-newfile").animate({top: "65px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-newfolder").show();
-		$("#nav-newfolder").animate({top: "120px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-setting").show();
-		$("#nav-setting").animate({top: "175px", opacity: 1}, 200, "easeInCubic");
-		$("#nav-exit").show();
-		$("#nav-exit").animate({top: "230px", opacity: 1}, 200, "easeInCubic");
-		return(false);
-	});
-	$("#nav-wrapper").mouseleave(function() {
-		$("#nav-newfile").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-newfile").hide()});
-		$("#nav-newfolder").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-newfolder").hide()});
-		$("#nav-setting").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-setting").hide()});
-		$("#nav-exit").animate({top: "10px", opacity: 0}, 200, "easeOutCubic", function() {$("#nav-exit").hide()});
-		return(false);
-	});
-	$("#nav-exit").click(function() {
-		$("#dialog-close").show();
-	});
-	$(".dialog-close").click(function() {
-		$(".dialog-mark").hide();
-	});
 });
