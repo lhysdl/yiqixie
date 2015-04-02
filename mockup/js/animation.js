300c7d6b93a12e949fd4499f9ae35e1a73532eae
liaoyiming <me@liaoyiming.com>
2015-04-02 22:30:04 +0800

    Add animation on index page

---
+++ mockup/js/animation.js
@@ -0,0 +1,38 @@
+// JavaScript Document
+function sleep (d) {
+  for (var t = Date.now(); Date.now() - t <= d;);
+}
+$(document).ready(function () {
+	var poem = new Array("当然是降龙十八掌！", "蛤蟆功表示不服！", "听听我的碧海潮生曲？");
+	var author = new Array("郭靖", "欧阳锋", "黄药师");
+	var color = new Array("#fb6d51", "#4fc1e9", "#9fd368");
+	var avatar = new Array("gj.jpg", "oyf.jpg", "hys.jpg");
+	var i = 0;
+	var j = 0;
+	var mode = 1;
+	var n = poem.length;
+	text = document.getElementById("animation-text");
+	function updatename(i) {
+		var name = document.getElementById("animation-name");
+		name.innerHTML = author[i];
+		name.style.backgroundColor = color[i];
+		name.style.backgroundImage = "url(./images/" + avatar[i] + ")";
+		var cursor = document.getElementById("animation-cursor");
+		cursor.style.backgroundColor = color[i];
+	}
+	function update() {
+		text.innerHTML = poem[i].substring(0, j);
+		if (j > poem[i].length) {
+			sleep(3000);
+			mode = -1;
+			updatename((i + 1) % n);
+		}
+		j += mode;
+		if (j < 0) {
+			mode = 1;
+			j = 0;
+			i = (i + 1) % n;
+		}
+	}
+	setTimeout(function () { updatename(0); setInterval(update, 100);}, 3000);
+});
